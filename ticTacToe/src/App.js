a7629047d2fb4c1e4266e93b06ffe202bd45d14d
Matt Doyle <doyle9214@gmail.com>
2024-05-21 15:41:48 +0100

    added reset button

--- ticTacToe/src/App.js
+++ ticTacToe/src/App.js
@@ -67,6 +67,11 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
+  function resetGame() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  }
+
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -87,6 +92,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <button onClick={resetGame}>Reset</button>
         <ol>{moves}</ol>
       </div>
     </div>
